ec03e2843720b393c0035526ff2856ad85c8c73b
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-19 20:45:57 -0400

    creating check guess function

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ let title = document.querySelector('h1');
 title.innerHTML = 'The secret number game';
 let paragraph = document.querySelector('p');
 paragraph.innerHTML = 'Choice a number between 1 and 10';
+
+function checkGuess() {
+    console.log('The button has been clicked');
+}
