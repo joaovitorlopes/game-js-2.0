56c0ca2e4dc829c2703bed5064c5deab9c37539f
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-19 20:39:38 -0400

    manipulate HTML texts

---
+++ app.js
@@ -0,0 +1,4 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'The secret number game';
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'Choice a number between 1 and 10';
