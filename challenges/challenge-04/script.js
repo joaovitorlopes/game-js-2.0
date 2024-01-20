85d13c19cef1130add77c2fb82cfc7b85b872020
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-19 23:25:29 -0400

    array challenge

---
+++ challenges/challenge-04/script.js
@@ -0,0 +1,23 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['João', 'Vitor', 'Fulano'];
+console.log(nomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let nomes2 = ['Beltrano', 'Cicrano', 'Brasil'];
+console.log(nomes2[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomes3 = ['Brasil', 'Argentina', 'Portugal'];
+console.log(nomes3[nomes3.length - 1]);
