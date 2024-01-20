ccff2310f5d2dd967c905c0e506aa8e0dd4ac726
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-19 21:09:36 -0400

    creating a function to change elements in HTML

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let title = document.querySelector('h1');
-title.innerHTML = 'The secret number game';
-let paragraph = document.querySelector('p');
-paragraph.innerHTML = 'Choice a number between 1 and 10';
+function changeElementInHTML(tag, text) {
+    let element = document.querySelector(tag);
+    element.innerHTML = text;
+}
+
+changeElementInHTML('h1', 'The secret number game');
+changeElementInHTML('p', 'Choice a number between 1 and 10');
+
 
 function checkGuess() {
     console.log('The button has been clicked');
